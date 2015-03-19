4fc6c42c5f327d8bd71e43f3cc55eea8527aa4bb
philiprad <philiprad@Phils-MacBook-Pro.local>
2015-03-19 18:46:03 +0000

    path filter

--- StackInTraffic/src/pathFiler/PathComparator.java
+++ StackInTraffic/src/pathFiler/PathComparator.java
@@ -1,21 +1,34 @@
 package pathFiler;
 
+import java.util.Comparator;
+
+import trafficInfrastructure.roadPath.Path;
+
 public class PathComparator implements Comparator<Path>{
-	
-	public int compare(Appointment a, Appointment b)
-	{
-	    if (a.getDay() < b.getDay())
-	        return -1;
 
-	    if (a.getDay() == b.getDay())
-	    {
-	        if (a.getStart() < b.getStart())
-	            return -1;
-	        if (a.getStart() > b.getStart())
-	            return 1;
-	        return 0;
-	    }
-
-	    return 1;
+	@Override
+	public int compare(Path o1, Path o2) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
+//	public int compare(Path a, Path b)
+//	{
+//	    if (a.() < b.getDay())
+//	        return -1;
+//
+//	    if (a.getDay() == b.getDay())
+//	    {
+//	        if (a.getStart() < b.getStart())
+//	            return -1;
+//	        if (a.getStart() > b.getStart())
+//	            return 1;
+//	        return 0;
+//	    }
+//
+//	    return 1;
+//	}
+
+	
+
+	
 }
--- StackInTraffic/src/pathFiler/PathFilter.java
+++ StackInTraffic/src/pathFiler/PathFilter.java
@@ -16,10 +16,10 @@ public class PathFilter {
 	
 	String fileName = "pathFilter2";
 	int Comparator;
-	Collections.sort(arrPath, new (Comparator)PathComparator();
-	Comparator<Path> c = Comparator<>arrPath.get(0).getPathPoints().size();
+	//Collections.sort(arrPath, new (Comparator)PathComparator();
+	//Comparator<Path> c = Comparator<>arrPath.get(0).getPathPoints().size();
 	arrPath = (ArrayList<Path>)FileRW.readObject(MainConfig.PATHS_PATH + "/" + fileName + MainConfig.PATH_SUFFIX);
-	arrPath.sort(c);
+	//arrPath.sort(c);
 	
 		for(Path path : arrPath){
 			System.out.println(path.getPathPoints().get(0).getX() + " - " + path.getPathPoints().get(0).getY() + " ("+ path.getPathPoints().size() + ") "+ " : " + path.getPathPoints().get(path.getPathPoints().size()-1).getX() + " - " + path.getPathPoints().get(path.getPathPoints().size()-1).getY()+ " ("+ path.getPathPoints().size() + ")");
