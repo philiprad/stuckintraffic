252b9fcd1960bab5e3817848383d9c658aad72f9
philiprad <philiprad@Phils-MacBook-Pro.local>
2015-03-20 16:43:47 +0000

    added filtering

--- StackInTraffic/src/pathFiler/PathComparator.java
+++ StackInTraffic/src/pathFiler/PathComparator.java
@@ -1,6 +1,10 @@
 package pathFiler;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collector;
 
 import trafficInfrastructure.roadPath.Path;
 
@@ -8,8 +12,58 @@ public class PathComparator implements Comparator<Path>{
 
 	@Override
 	public int compare(Path o1, Path o2) {
+		
+		//if(o1.getPathPoints().get(0).getX()==o2.getPathPoints().get(0). 
+		//	o1.getPathPoints().get(0).getY()==o2.getPathPoints().get(0).getY())// &&
+			//o1.getPathPoints().get(o1.getPathPoints().size()-1);
+//		ArrayList<Path> pp = new ArrayList<Path>();
+//		pp.add(o1);
+//		pp.add(o2);
+//		Collections.sort(pp);
+			int crit1 = o1.getPathPoints().get(0).getX() - o2.getPathPoints().get(0).getX();
+			if(crit1 ==0)
+				crit1 = o1.getPathPoints().get(0).getX() - o2.getPathPoints().get(0).getX();
+			if(crit1 ==0)
+				crit1 = o1.getPathPoints().get(o1.getPathPoints().size()-1).getX() - o2.getPathPoints().get(o2.getPathPoints().size()-1).getX();
+			if(crit1==0)
+				crit1 = o1.getPathPoints().get(o1.getPathPoints().size()-1).getY()- o2.getPathPoints().get(o2.getPathPoints().size()-1).getY();
+			if(crit1==0)
+				crit1=o1.getPathPoints().size()-o2.getPathPoints().size();
+			//if(crit1 ==0)
+				//crit1=  o1.getPathPoints().get(o1.getPathPoints().size()-1).getY()- o2.getPathPoints().get(o2.getPathPoints().size()-1).getY();
+
+			return crit1;
+			
+				
+			//	o1.getPathPoints().get(o1.getPathPoints().size()-1).getY()==o2.getPathPoints().get(o2.getPathPoints().size()-1).getY()){
+//				return 0;
+//			}else{
+//				return 1;
+//			}
+					//(o2.getPathPoints().get(o2.getPathPoints().size()-1));
+//			
+//			boolean comparare2 = o1.getPathPoints().get(0).equals(o2.getPathPoints().get(0));
+//			if(comparare & comparare2)
+//				return 0;
+//			else
+//				return 1;
+			//return ;
+			//o2.getPathPoints().get(o2.getPathPoints().size()-1).getX() //&& 
+			//o1.getPathPoints().get(o1.getPathPoints().size()-1).getY()==o2.getPathPoints().get(o2.getPathPoints().size()-1).getY()?0:-1;
+		
+//			return (this.arrPathPoint.get(this.arrPathPoint.size()-1).getX()==((Path)o).getPathPoints().get(((Path)o).getPathPoints().size()-1).getX()  &&
+//					this.arrPathPoint.get(this.arrPathPoint.size()-1).getY()==((Path)o).getPathPoints().get(((Path)o).getPathPoints().size()-1).getY() &&
+//							this.arrPathPoint.get(0).getX()==((Path)o).getPathPoints().get(0).getX() &&
+//							this.arrPathPoint.get(0).getY()==((Path)o).getPathPoints().get(0).getY())?0:1;
+			
+			
+		//	return o1.getPathPoints().size() - o2.getPathPoints().size();
+//		}else{
+//			return 0;
+//		}
+		
+		
 		// TODO Auto-generated method stub
-		return 0;
 	}
 //	public int compare(Path a, Path b)
 //	{
--- StackInTraffic/src/pathFiler/PathFilter.java
+++ StackInTraffic/src/pathFiler/PathFilter.java
@@ -6,6 +6,7 @@ import java.util.Comparator;
 
 import main.MainConfig;
 import trafficInfrastructure.roadPath.Path;
+import trafficInfrastructure.roadPath.PathPoint;
 import util.FileRW;
 
 public class PathFilter {
@@ -14,16 +15,33 @@ public class PathFilter {
 	
 	public static void main (String[] args){
 	
-	String fileName = "pathFilter2";
+	String fileName = "pathFilter3";
 	int Comparator;
 	//Collections.sort(arrPath, new (Comparator)PathComparator();
 	//Comparator<Path> c = Comparator<>arrPath.get(0).getPathPoints().size();
 	arrPath = (ArrayList<Path>)FileRW.readObject(MainConfig.PATHS_PATH + "/" + fileName + MainConfig.PATH_SUFFIX);
 	//arrPath.sort(c);
-	
+	arrPath.sort(new PathComparator());
+	//Collections.sort(arrPath);
+	//0 - 115 (381)  : 185 - 349 (381)
+	//0 - 115 (550)  : 164 - 0 (550)
+	//0 - 115 (273)  : 164 - 0 (273)
+	//Collections.sort(arrPath, new PathComparator());
 		for(Path path : arrPath){
-			System.out.println(path.getPathPoints().get(0).getX() + " - " + path.getPathPoints().get(0).getY() + " ("+ path.getPathPoints().size() + ") "+ " : " + path.getPathPoints().get(path.getPathPoints().size()-1).getX() + " - " + path.getPathPoints().get(path.getPathPoints().size()-1).getY()+ " ("+ path.getPathPoints().size() + ")");
-	
+			
+			System.out.println( path.getPathPoints().get(0).getX() + " - " + path.getPathPoints().get(0).getY() + " ("+ path.getPathPoints().size() + ") "+ " : " + path.getPathPoints().get(path.getPathPoints().size()-1).getX() + " - " + path.getPathPoints().get(path.getPathPoints().size()-1).getY()+ " ("+ path.getPathPoints().size() + ")");
+//			System.out.println("-" + path.getPathPoints().get(1).getX() + " - " + path.getPathPoints().get(0).getY() + " ("+ path.getPathPoints().size() + ") "+ " : " + path.getPathPoints().get(path.getPathPoints().size()-2).getX() + " - " + path.getPathPoints().get(path.getPathPoints().size()-2).getY()+ " ("+ path.getPathPoints().size() + ")");
+
+			if(path.getPathPoints().get(0).getX()==0 && path.getPathPoints().get(0).getY()==115 && path.getPathPoints().get(path.getPathPoints().size()-1).getX()==499 && path.getPathPoints().get(path.getPathPoints().size()-1).getY()==265){
+			
+				for (int i =0; i< path.getPathPoints().size();i++ ){//PathPoint pp :path.getPathPoints()){
+				System.out.print(path.getPathPoints().get(i).getX()+"    ");
+			}
+			System.out.print("\n");
+			for (int i =0; i< path.getPathPoints().size();i++ ){//PathPoint pp :path.getPathPoints()){
+				System.out.print(path.getPathPoints().get(i).getY()+"    ");
+			}
+			}
 		}
 	}
 }
